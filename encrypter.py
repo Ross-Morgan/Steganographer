5b7006b85c74d7e7041c6b503a82f5ec13514e00
Ross-Morgan <68851224+Ross-Morgan@users.noreply.github.com>
2022-01-06 10:00:14 +0000

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -4,39 +4,36 @@ from typing import Optional
 
 class ImageEncrypter:
     """Handle writing and reading of an image"""
-    def __init__(self):
-        pass
-
     @property
     @staticmethod
     def file_contents(filename) -> bytes:
-        return open(filename, "rb").read()
+        return open(filename, "rb", encoding="utf-8").read()
 
     @staticmethod
     def get_offset(data: bytes):
         return data.index(bytes.fromhex("FFD9")) + 2
 
-    def overwrite_message(self,msg:str):
+    def overwrite_message(self, msg: str):
         # Remove message from file
         self.reset_image()
 
         # Write message to file
         self.write_message(msg)
 
-    def write_message(self, filename:str, msg:str, print_message:bool=False):
+    def write_message(self, filename: str, msg: str, print_message: bool = False):
         if filename is None:
             return
 
         # Open file for appending
-        with open(filename, "ab") as file:
+        with open(filename, "ab", encoding="utf-8") as file:
             # Write encoded message to file
             file.write(bytes(msg, encoding="utf-8"))
 
         if print_message:
             print(f"Written message: '{msg}'")
 
-    def message_length(self, filename):
-        file_contents = open(filename, "rb").read()
+    def message_length(self, filename: str):
+        file_contents = open(filename, "rb", encoding="utf-8").read()
         file_length = len(file_contents)
         offset = self.get_offset(file_contents)
 
@@ -47,7 +44,7 @@ class ImageEncrypter:
             return ''
 
         # Open file for reading
-        with open(filename, "rb") as file:
+        with open(filename, "rb", encoding="utf-8") as file:
             try:
                 # Move cursor to start of message
                 file.seek(self.get_offset(file.read()))
@@ -66,7 +63,7 @@ class ImageEncrypter:
             return
 
         # Open file for writing
-        file = open(filename, "rb+")
+        file = open(filename, "rb+", encoding="utf-8")
 
         # Truncate file to len(file) - n bytes
         file.truncate(len(file.read())- self.message_length(filename))
@@ -76,11 +73,10 @@ class ImageEncrypter:
 
 
 def main():
-    galaxy = ImageEncrypter()
-    galaxy.reset_image()
-    galaxy.read_message(print_message=True)
+    enc = ImageEncrypter()
+    enc.reset_image()
+    enc.read_message(print_message=True)
 
-if __name__ == "__main__":
+
+    if __name__ == "__main__":
     main()
-
-    # print(open("galaxy.jpg", "rb").read().index(bytes.fromhex("FFD9")))
